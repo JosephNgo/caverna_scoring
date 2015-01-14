b200befac760ec7ee5ec1944d8e102d076ddbebe
Joe Ngo <joengo83@gmail.com>
2015-01-13 23:12:32 -0800

    refactored adding tile scores and page tabbing

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,9 @@
-
-
+$(".cust-btn").click(function(){
+  $(".tab-pane").hide();
+  var toggleClick = $(this);
+  var toggleDiv = "#" + $(this).data("slideId");
+  $(toggleDiv).slideToggle(1000);
+})
 
 var subtract = function(clicked_id){
   mystring=clicked_id.replace(/^sub_/,"");
@@ -17,146 +21,6 @@ var add = function(clicked_id){
   updateScore();
 };
 
-var getMultiplier = function(){
-  return 1;
-}
-
-var dog = function(){
-  subvalue = parseInt(document.getElementById("dog").value);
-  return subvalue;
-}
-
-var sheep = function(){
-  subvalue = parseInt(document.getElementById("sheep").value);
-  if (subvalue < 1){
-    return -2;
-  }
-  else {
-    return subvalue;
-  }
-}
-
-var donkey = function(){
-  subvalue = parseInt(document.getElementById("donkey").value);
-  if (subvalue < 1){
-    return -2;
-  }
-  else {
-    return subvalue;
-  }
-}
-
-var boar = function(){
-  subvalue = parseInt(document.getElementById("boar").value);
-  if (subvalue < 1){
-    return -2;
-  }
-  else {
-    return subvalue;
-  }
-}
-
-var cattle = function(){
-  subvalue = parseInt(document.getElementById("cattle").value);
-  if (subvalue < 1){
-    return -2;
-  }
-  else {
-    return subvalue;
-  }
-}
-
-var grain = function(){
-  subvalue = parseInt(document.getElementById("grain").value);
-  return Math.round(subvalue/2);
-}
-
-var vegetable = function(){
-  subvalue = parseInt(document.getElementById("vegetable").value);
-  return subvalue;
-}
-
-var ruby = function(){
-  subvalue = parseInt(document.getElementById("ruby").value);
-  return subvalue;
-}
-
-var gold = function(){
-  subvalue = parseInt(document.getElementById("gold").value);
-  return subvalue;
-}
-
-var begging = function(){
-  subvalue = parseInt(document.getElementById("begging").value);
-  return -(subvalue*3);
-}
-
-var dwarf = function(){
-  subvalue = parseInt(document.getElementById("dwarf").value);
-  return subvalue*3;
-}
-
-var dwelling = function(){
-  subvalue = parseInt(document.getElementById("dwelling").value);
-  return subvalue*2;
-  updateScore();
-}
-
-var simpleDwelling = function(){
-  if(document.getElementById("simpleDwelling").checked == true){
-    return 0;
-  }
-  else {
-    return 0;
-  }
-}
-
-document.getElementById("simpleDwelling").addEventListener("click", function(){
-  simpleDwelling();
-  updateScore();
-});
-
-var mixedDwelling = function(){
-  if(document.getElementById("mixedDwelling").checked == true){
-    return 4;
-  }
-  else {
-    return 0;
-  }
-}
-
-document.getElementById("mixedDwelling").addEventListener("click", function(){
-  mixedDwelling();
-  updateScore();
-});
-
-var couplesDwelling = function(){
-  if(document.getElementById("couplesDwelling").checked == true){
-    return 5;
-  }
-  else {
-    return 0;
-  }
-}
-
-// document.getElementById("couplesDwelling").addEventListener("click", function(){
-//   couplesDwelling();
-//   updateScore();
-// });
-
-var additionalDwelling = function(bonusValue){
-  if(document.getElementById("additionalDwelling").checked == true){
-    return 5;
-  }
-  else {
-    return 0;
-  }
-}
-
-// document.getElementById("additionalDwelling").addEventListener("click", function(){
-//   additionalDwelling();
-//   updateScore();
-// });
 var checkboxStatus = function(tileId, bonusValue){
   if(document.getElementById(tileId).checked == true){
     console.log("checked");
@@ -166,11 +30,6 @@ var checkboxStatus = function(tileId, bonusValue){
     console.log("unchecked");
     addPoints(bonusValue*(-1));
   }
-}
-
-var addPoints = function(bonusValue){
-  console.log("add points:" + bonusValue);
-  updateScore2(bonusValue);
 }
 
 //function to find the id of 'this' mini-tile
@@ -185,58 +44,46 @@ $(".mini-tile").click(function(){
 
 })
 
-document.getElementById("carpenter").addEventListener("click", function(){
-  addFive("carpenter");
-
-});
-
-document.getElementById("blacksmith").addEventListener("click", function(){
-  addFive("blacksmith");
-
-});
-
 var tileMap = {
-  "dog": [1,.5],
-  "sheep": [1,-1],
-  "dwarf": [3,0],
-  "dwelling": [2,0]
+  "dog": [0,1], // "inputID": [multiplier under 1, multiplier over 1]
+  "sheep": [-1,1],
+  "donkey": [-1,1],
+  "boar": [-1,1],
+  "cattle": [-1,1],
+  "grain": [0, 0.5],
+  "vegetable": [0, 1], 
+  "ruby": [0,1],
+  "gold": [0, 1],
+  "begging": [0, -3],
+  "dwarf": [0,3],
+  "dwelling": [0,2]
 };
 
 var tileData = function(){
   var sum = 0;
-
   $(".tileData").each(function(){
     var id = $(this).attr('id');
-    if (parseInt($(this).val())<1){
-      var penalty = tileMap[id][1];
-      sum += penalty;
+    var inputData = parseInt($(this).val());
+    var tileScore = 0;
+    if (inputData < 1){
+      var penalty = tileMap[id][0];
+      tileScore = penalty;
+      sum += tileScore;
     }
     else {
-      var multiplier = tileMap[id][0];
-      sum += (parseInt($(this).val())*multiplier);
+      var multiplier = tileMap[id][1];
+      tileScore = inputData * multiplier;
+      sum += tileScore;
     }
+    
   })
-  return sum;
+  return Math.round(sum);
 }
 
-var placeholderScore = 0;
-
-var updateScore2 = function(scoreData) {
-  placeholderScore += parseInt(scoreData);
-  
-  
-  if (placeholderScore !== placeholderScore) {
-    return 0;
-  }
-  else {
-    return placeholderScore;
-  }
-  console.log("updateScore2:" + placeholderScore);
-}
-
+tileData();
 
 var updateScore = function(){
-  var totalScore = updateScore2() + dog() + sheep() + donkey() + boar() + cattle() + grain() + vegetable() + ruby() +gold() + begging() + dwarf() + dwelling() + simpleDwelling() + mixedDwelling() + couplesDwelling() + additionalDwelling();
+  var totalScore = tileData();
   updateScoreHTML(totalScore);
   return totalScore;
 }
